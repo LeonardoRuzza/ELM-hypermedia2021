6949f9085226b200e5ca7d65c10430ea74681e9a
LeonardoRuzza <10608001@polimi.it>
2021-07-02 12:06:39 +0200

    Minor fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Website of the ELM Corporation for the Hypermedia 2021 project',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
